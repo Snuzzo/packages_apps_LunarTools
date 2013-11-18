7e67e50df6a84cf06ff9e5a56f90801df9ddb0b9
Snuzzo <jrracinfan@gmail.com>
2013-11-18 16:56:33 -0500

    Fix build copy errors of busybox extended scripting.

--- Android.mk
+++ Android.mk
@@ -10,4 +10,6 @@ LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
 include $(BUILD_PREBUILT)
 
+$(shell mkdir -p $(OUT)/system)
+$(shell mkdir -p $(OUT)/system/bin)
 $(shell cp -rf $(LOCAL_PATH)/prebuilt/* $(OUT)/system/bin/)
